0e7ff375676508749dc30b2cabdb710970d84ee3
vera yudovina <vera.yudovina@hyland.com>
2021-02-23 10:55:41 -0500

    Initial Commit for CQRS using MediatR

--- CleanArch/CleanArch.Mvc/Controllers/CourseController.cs
+++ CleanArch/CleanArch.Mvc/Controllers/CourseController.cs
@@ -11,7 +11,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CleanArch.Mvc.Controllers
 {
-    //[Authorize]
     public class CourseController : Controller
     {
         private ICourseService _courseService;
