45aa3bc3b22cc6d877a9eb67b2e7ee736d16ccfc
vera yudovina <vera.yudovina@hyland.com>
2021-02-24 13:40:24 -0500

    Added comands, handlers, updated courserepository, cleanedup IoC

---
+++ CleanArch/CleanArch.Domain/Commands/CreateCourseCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Domain.Commands
+{
+    public class CreateCourseCommand : CourseCommand
+    {
+        public CreateCourseCommand (string name, string descr, string imageurl)
+        {
+            Name = name;
+            Description = descr;
+            ImageUrl = imageurl;
+        }
+    }
+}
