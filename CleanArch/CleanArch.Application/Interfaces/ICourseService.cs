a0d7c6b6caf0db20d6a255e75d66995d28a76653
vera yudovina <vera.yudovina@hyland.com>
2021-05-25 10:32:07 -0400

    Create

--- CleanArch/CleanArch.Application/Interfaces/ICourseService.cs
+++ CleanArch/CleanArch.Application/Interfaces/ICourseService.cs
@@ -8,5 +8,6 @@ namespace CleanArch.Application.Interfaces
     public interface ICourseService
     {
         CourseViewModel GetCourses();
+        void CreateCourse(CourseViewModel courseViewModel);
     }
 }
